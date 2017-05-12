f946c36dadad5a176bde9ebe6864ebb6b1753110
Jack Grahl <jack.grahl@gmail.com>
2017-05-12 18:27:45 +0000

    Commands to compile C++ module.

--- pyQuantuccia/setup.py
+++ pyQuantuccia/setup.py
@@ -1,7 +1,13 @@
 import setuptools
 
+qu_ext = setuptools.Extension(
+    'quantuccia',
+    sources = ['pyQuantuccia.cpp']
+)
+
 setuptools.setup(
     name='pyQuantuccia',
     version='0.1.0',
-    packages=['pyQuantuccia']
+    packages=['pyQuantuccia'],
+    ext_modules=[qu_ext]
 )
