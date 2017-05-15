f4608145d783d1027f85f973b31787a94d0d7182
Jack Grahl <jack.grahl@gmail.com>
2017-05-15 22:21:42 +0000

    Add name and email.

--- pyQuantuccia/setup.py
+++ pyQuantuccia/setup.py
@@ -8,6 +8,8 @@ qu_ext = setuptools.Extension(
 
 setuptools.setup(
     name='pyQuantuccia',
+    author='Jack Grahl',
+    author_email='jack.grahl@gmail.com',
     version='0.1.0',
     packages=['pyQuantuccia'],
     ext_modules=[qu_ext]
