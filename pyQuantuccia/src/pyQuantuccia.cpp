0131240f5f2dc70c59679d636bb228651a4b5ad6
Jack Grahl <jack.grahl@gmail.com>
2017-05-17 23:45:51 +0000

    Re-arrange the order things are defined in this file.

--- pyQuantuccia/src/pyQuantuccia.cpp
+++ pyQuantuccia/src/pyQuantuccia.cpp
@@ -1,19 +1,19 @@
 #include <Python.h>
 #include "Quantuccia/ql/time/calendars/unitedstates.hpp"
 
- PyObject* PyInit_pyQuantuccia(void){
-     PyObject *m;
-     m = PyInitModule("quantuccia", QuantucciaMethods);
-     return m;
- }
-
+static PyObject*
+get_holiday_date(PyObject *self, PyObject *args)
+{
+    return NULL;
+}
+ 
 static PyMethodDef QuantucciaMethods[] = {
 	{"get_holiday_date", (PyCFunction)get_holiday_date, METH_VARARGS, NULL},
 	{NULL,     NULL}        /* Sentinel */
 };
 
- static PyObject*
- get_holiday_date(PyObject *self, PyObject *args)
- {
-     return NULL;
- }
+PyObject* PyInit_pyQuantuccia(void){
+    PyObject *m;
+    m = PyCreate_Module("quantuccia", QuantucciaMethods);
+    return m;
+}
