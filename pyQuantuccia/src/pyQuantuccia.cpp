6a36f7178ad5f5d66b7fae91f26f314b5f45dd7a
Jack Grahl <jack.grahl@gmail.com>
2017-05-17 23:41:19 +0000

    Attempt to initialize module with an empty function.

--- pyQuantuccia/src/pyQuantuccia.cpp
+++ pyQuantuccia/src/pyQuantuccia.cpp
@@ -2,5 +2,18 @@
 #include "Quantuccia/ql/time/calendars/unitedstates.hpp"
 
  PyObject* PyInit_pyQuantuccia(void){
+     PyObject *m;
+     m = PyInitModule("quantuccia", QuantucciaMethods);
+     return m;
+ }
+
+static PyMethodDef QuantucciaMethods[] = {
+	{"get_holiday_date", (PyCFunction)get_holiday_date, METH_VARARGS, NULL},
+	{NULL,     NULL}        /* Sentinel */
+};
+
+ static PyObject*
+ get_holiday_date(PyObject *self, PyObject *args)
+ {
      return NULL;
  }
