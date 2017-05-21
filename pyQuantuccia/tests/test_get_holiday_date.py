6943987a728052277a12d634896addd355a1b972
Jack Grahl <jack.grahl@gmail.com>
2017-05-21 08:35:25 +0000

    Try to import quantuucia directly.

--- pyQuantuccia/tests/test_get_holiday_date.py
+++ pyQuantuccia/tests/test_get_holiday_date.py
@@ -1,9 +1,8 @@
-#  from pyQuantuccia import quantuccia
+import quantuccia
 
 
 def test_get_holiday_date():
     """ At the moment the only thing this function
         can do is return NULL.
     """
-    #  assert(quantuccia.get_holiday_date() is None)
-    pass
+    assert(quantuccia.get_holiday_date() is None)
