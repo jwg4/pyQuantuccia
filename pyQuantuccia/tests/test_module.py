949c723e040259ed956b4ca1a9be45fb6c297d74
Jack Grahl <jack.grahl@gmail.com>
2017-05-24 05:55:38 +0000

    Remove this file, no longer needed.

--- pyQuantuccia/tests/test_module.py
+++
@@ -1,9 +0,0 @@
-import pyQuantuccia
-
-
-def test_not_a_real_test():
-    _dir = str(pyQuantuccia.__dir__())
-    _dict = str(pyQuantuccia.__dict__)
-    s = " ".join([_dict, _dir])
-    print(s)
-    assert(s == "")
