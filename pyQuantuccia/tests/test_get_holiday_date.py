e7a7b126826413fa1dc9a61327eb4d50af3acd51
Jack Grahl <jack.grahl@gmail.com>
2017-05-25 22:30:48 +0000

    Update the docstring.

--- pyQuantuccia/tests/test_get_holiday_date.py
+++ pyQuantuccia/tests/test_get_holiday_date.py
@@ -2,7 +2,7 @@ import quantuccia
 
 
 def test_easter_monday():
-    """ At the moment the only thing this function
-        can do is return NULL.
+    """ Check a single year to make sure we can find 
+        the date of Catholic Easter.
     """
     assert(quantuccia.easter_monday(2017) == 76)
