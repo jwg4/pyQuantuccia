f5da8fe3d65aeaf79e4dcabbdcd16af766ae5a0a
Jack Grahl <jack.grahl@gmail.com>
2017-05-25 23:44:16 +0000

    Flake8 fixes

--- pyQuantuccia/tests/test_get_holiday_date.py
+++ pyQuantuccia/tests/test_get_holiday_date.py
@@ -1,8 +1,10 @@
+from datetime import date
+
 import calendar
 
 
 def test_united_kingdom_is_business_day():
-    """ Check a single day to see that we 
+    """ Check a single day to see that we
     can identify holidays.
     """
     assert(calendar.united_kingdom_is_business_day(date(2017, 4, 17)) is False)
