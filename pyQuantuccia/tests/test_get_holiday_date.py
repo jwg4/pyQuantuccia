f9cdc9ecf4c78b135ac5b1da0df91851a64dda08
Jack Grahl <jack.grahl@gmail.com>
2017-06-19 20:11:29 +0000

    Spacing.

--- pyQuantuccia/tests/test_get_holiday_date.py
+++ pyQuantuccia/tests/test_get_holiday_date.py
@@ -9,6 +9,7 @@ def test_united_kingdom_is_business_day_identifies_a_holiday():
     """
     assert(calendar.united_kingdom_is_business_day(date(2017, 4, 17)) is False)
 
+
 def test_united_kingdom_is_business_day_identifies_a_business_day():
     """ Check a single day to see that we
     can identify holidays.
