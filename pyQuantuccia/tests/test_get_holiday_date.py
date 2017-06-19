b24f79d05c3682a1c21b7354d3fabd09f871cce9
Jack Grahl <jack.grahl@gmail.com>
2017-06-19 20:28:23 +0000

    Slightly more meaningful comments.

--- pyQuantuccia/tests/test_get_holiday_date.py
+++ pyQuantuccia/tests/test_get_holiday_date.py
@@ -4,14 +4,12 @@ from pyQuantuccia import calendar
 
 
 def test_united_kingdom_is_business_day_identifies_a_holiday():
-    """ Check a single day to see that we
-    can identify holidays.
+    """ Check a single day which isn't a holiday.
     """
     assert(calendar.united_kingdom_is_business_day(date(2017, 4, 17)) is False)
 
 
 def test_united_kingdom_is_business_day_identifies_a_business_day():
-    """ Check a single day to see that we
-    can identify holidays.
+    """ Check a single day which is a holiday.
     """
     assert(calendar.united_kingdom_is_business_day(date(2017, 6, 19)) is True)
