8e773cb6df43c697e0d088813cf143fff9453624
Jack Grahl <jack.grahl@gmail.com>
2017-06-19 20:27:54 +0000

    Add a test which directly calls the c function.

---
+++ pyQuantuccia/tests/test_quantuccia.py
@@ -0,0 +1,13 @@
+import pytest
+
+from pyQuantuccia import quantuccia
+
+
+def test_function_fails_correctly_on_bad_input():
+    """
+    If we send in something which isn't three 
+    appropriate ints, something reasonable should
+    happen.
+    """
+    with pytest.raises(Exception):
+        quantuccia.united_kingdom_is_business_day(1, 2, 3, 4)
