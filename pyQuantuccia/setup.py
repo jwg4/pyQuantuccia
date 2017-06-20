7b271cc37f0b0f12f17fa794f72f15f6e4f2f3b1
Jack Grahl <jack.grahl@gmail.com>
2017-06-20 23:44:24 +0000

    A hack to be able to build this stuff on windows.

--- pyQuantuccia/setup.py
+++ pyQuantuccia/setup.py
@@ -1,8 +1,16 @@
+import platform
 import setuptools
+
+if platform.system() == 'Windows':
+    extra_dirs = [
+        "C:\\Program Files (x86)\\Windows Kits\\10\\include\\10.0.10240.0\\ucrt" # noqa
+    ]
+else:
+    extra_dirs = []
 
 qu_ext = setuptools.Extension(
     'quantuccia',
-    include_dirs=['src/Quantuccia'],
+    include_dirs=['src/Quantuccia'] + extra_dirs,
     sources=['src/pyQuantuccia.cpp'],
     headers=[]
 )
