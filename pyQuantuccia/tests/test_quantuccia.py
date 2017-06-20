629766f8e5fcdb8bb89eebd1729286592dda3b92
Jack Grahl <jack.grahl@gmail.com>
2017-06-20 23:48:44 +0000

    Correctly skip this test, and dont attempt the import.

--- pyQuantuccia/tests/test_quantuccia.py
+++ pyQuantuccia/tests/test_quantuccia.py
@@ -1,14 +1,14 @@
 import pytest
 
-from pyQuantuccia import quantuccia
 
-
-@pytest.skip
+@pytest.mark.skip("We can't load this module at the moment")
 def test_function_fails_correctly_on_bad_input():
     """
     If we send in something which isn't three
     appropriate ints, something reasonable should
     happen.
     """
+    from pyQuantuccia import quantuccia
+
     with pytest.raises(Exception):
         quantuccia.united_kingdom_is_business_day(1, 2, 3, 4)
