b9f95972f5ce4a1b9da1230d236f25c5b38af2cd
Jack Grahl <jack.grahl@gmail.com>
2017-06-23 22:30:10 +0000

    Whitespace.

--- pyQuantuccia/tests/test_calendars.py
+++ pyQuantuccia/tests/test_calendars.py
@@ -2,7 +2,7 @@ from pyQuantuccia import py_calendar
 
 
 def test_can_get_TARGET_calendar_wrapper():
-    """ Check we can create a POPO which 
+    """ Check we can create a POPO which
         contains a reference to a C++ object.
     """
     target_cal = py_calendar.TargetCalendar()
